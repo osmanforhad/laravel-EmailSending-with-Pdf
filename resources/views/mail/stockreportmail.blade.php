7b3c5ea8650b7d03c5a8bef55c0b816c3076c5ae
osmanforhad <osmanforhad93@gmail.com>
2021-12-26 21:20:30 +0600

    create mail class and view with model migration

---
+++ resources/views/mail/stockreportmail.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Email With an PDF Attachment</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  </head>
+  <body>
+<div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-sm-12 m-auto">
+                <p>Dear Admin,</p>
+                <br/>
+                <p>Attach with the stock report.</p>
+                <br/>
+                <p> Best Regards</p>
+                <p> Inventory Team </p>
+            </div>
+        </div>
+    </div>
+  </body>
+</html>
